61803a0d61460cd4b76dab824807fb083bb53400
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-03-23 11:49:21 +0100

    Update Register.php

--- src/recurring_message/refaltor/Register.php
+++ src/recurring_message/refaltor/Register.php
@@ -11,7 +11,9 @@ class Register extends PluginBase{
         $this->saveDefaultConfig();
         foreach ($this->getConfig()->get("message") as $values){
             $message = explode(":", $values);
-            $this->getScheduler()->scheduleDelayedRepeatingTask(new msgTask($message[0], $message[1], $this), ($message[1] * 20), 20);
+            $this->getScheduler()->scheduleDelayedRepeatingTask(new msgTask($message[0], $message[1], $this), $this->secToTick($message[1]), 20);
         }
     }
+    public function secToTick(int $sec): int { return $sec * 20; }
+
 }
